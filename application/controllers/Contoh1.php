68b4a475c61262a5c6dc1561479fe50ceb5ae054
kianaghies <aghiesn330@gmail.com>
2023-03-31 14:34:47 +0700

    Menambahkan Tugas Pertemuan kedua

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalan</h1>";
-        echo "Nama saya Imam
-                Saya Tinggal di daerah Ciputat
-                olahraga yang saya sukai adalah Bulutangkis";
+        echo "Nama saya Aghies Nokiandi
+                Saya Tinggal di daerah Bintaro
+                Olahraga yang saya sukai ??????";
     }
 }
 ?>
